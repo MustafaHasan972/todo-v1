76457fc9149ade629657d1f9c979ccd79f931886
Mustafa Hasan <mustafa2k8972@gmail.com>
2024-06-01 01:08:23 +0530

    Updated rows

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ function App() {
       <h1>Todo App</h1>
       <div className="container text-center">
         <div className="row">
-          <div className="col-8">col-8</div>
           <div className="col-4">col-4</div>
+          <div className="col-4">col-4</div>
+          <div className="col-2">col-2</div>
         </div>
       </div>
     </center>
