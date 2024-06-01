42f85ba86e4d486a14caecafaadb3626bc068f0c
Mustafa Hasan <mustafa2k8972@gmail.com>
2024-06-01 23:14:02 +0530

    Created a UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,37 @@ function App() {
       <h1>Todo App</h1>
       <div className="container text-center">
         <div className="row">
-          <div className="col-4">col-4</div>
-          <div className="col-4">col-4</div>
-          <div className="col-2">col-2</div>
+          <div className="col-4">
+            <input type="text" placeholder="Enter Task" />
+          </div>
+          <div className="col-4">
+            <input type="date" placeholder="dd/mm/yyyy" />
+          </div>
+          <div className="col-2">
+            <button type="button" className="btn btn-success">
+              Success
+            </button>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-4">Buy Milk</div>
+          <div className="col-4">01/06/2024</div>
+          <div className="col-2">
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </div>
+
+          <div className="row">
+            <div className="col-4">Buy Milk</div>
+            <div className="col-4">01/06/2024</div>
+            <div className="col-2">
+              <button type="button" className="btn btn-danger">
+                Delete
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </center>
