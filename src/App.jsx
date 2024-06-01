d5a117f48609e01e51806ac81c0b9a7618e57a45
Mustafa Hasan <mustafa2k8972@gmail.com>
2024-06-01 23:18:18 +0530

    Added a component for Heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import AppName from "./components/AppName";
+
 function App() {
   return (
     <center className="todo-container">
-      <h1>Todo App</h1>
+      <AppName></AppName>
       <div className="container text-center">
         <div className="row">
           <div className="col-4">
@@ -27,7 +29,7 @@ function App() {
           </div>
 
           <div className="row">
-            <div className="col-4">Buy Milk</div>
+            <div className="col-4">Read a Novel</div>
             <div className="col-4">01/06/2024</div>
             <div className="col-2">
               <button type="button" className="btn btn-danger">
